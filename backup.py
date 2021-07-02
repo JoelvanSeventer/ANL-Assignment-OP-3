dc66e5100729ff4b04a833b54a7f35cfd5bf9f00
DanIsVis <danielvisser.nl@gmail.com>
2021-07-02 16:22:54 +0200

    namen verandert

--- backup.py
+++ backup.py
@@ -2,16 +2,16 @@ import shutil
 
 class Catalog():
     def __init__(self):
-        self.backup_path = 'database/backup/'
+        self.backup_path = 'data/backup/'
 
     def makeBackup(self):
-        shutil.copy('database/books.json', self.backup_path + 'backupbooks.json')
-        shutil.copy('database/customers.csv', self.backup_path + 'backupcustomers.csv')
-        shutil.copy('database/loans.json', self.backup_path + 'backuploans.json')
-        shutil.copy('database/bookcopies.json', self.backup_path + 'backupbookcopies.json')
+        shutil.copy('data/books.json', self.backup_path + 'backupbooks.json')
+        shutil.copy('data/customers.csv', self.backup_path + 'backupcustomers.csv')
+        shutil.copy('data/loans.json', self.backup_path + 'backuploans.json')
+        shutil.copy('data/bookcopies.json', self.backup_path + 'backupbookcopies.json')
 
     def restoreDB(self):
-        shutil.copy(self.backup_path + 'backupbooks.json', 'database/books.json')
-        shutil.copy(self.backup_path + 'backupcustomers.csv', 'database/customers.csv')
-        shutil.copy(self.backup_path + "backuploans.json", 'database/loans.json')
-        shutil.copy(self.backup_path + 'backupbookcopies.json', 'database/bookcopies.json')
+        shutil.copy(self.backup_path + 'backupbooks.json', 'data/books.json')
+        shutil.copy(self.backup_path + 'backupcustomers.csv', 'data/customers.csv')
+        shutil.copy(self.backup_path + "backuploans.json", 'data/loans.json')
+        shutil.copy(self.backup_path + 'backupbookcopies.json', 'data/bookcopies.json')
