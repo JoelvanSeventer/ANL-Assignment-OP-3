6b1fd219fab799dbb6dfbd020b80e49ba5fef75d
DanIsVis <danielvisser.nl@gmail.com>
2022-06-11 23:31:47 +0200

    fixed noobcode

--- person.py
+++ person.py
@@ -38,7 +38,7 @@ class Person():
             index = len(csvdata)
         data = f"\n{str(index)};{givenname};{surname};{streetaddress};{zipcode};{city};{emailaddress};{username};{password};{telephonenumber}"
 
-        with open("data/members.csv", "a") as f:
+        with open("data/members.csv", "a", newline='') as f:
             f.write(data)
 
     #Search member 
@@ -56,48 +56,88 @@ class Person():
             
         terms = ['Givenname', 'Surname', 'Streetaddress', 'Zipcode', 'City', 'Emailaddress', 'Username', 'Telephonenumber']
         
-        usercheck = input(f"\nFill in the {terms[int(check) - 1]}:\n").lower()
-        for user in csvdata:
-            split_user = user[0].split(';') 
-            if check == "1" and usercheck == split_user[1].lower():
-                    print(split_user[0] + ". " + split_user[1] + " " + split_user[2] + "\nStreetaddress: " + split_user[3] + "\nZipcode: " + split_user[4] + "\nCity: " + split_user[5] + "\nEmailaddress: " + split_user[6] + "\nUsername: " + split_user[7] + "\nTelephonenumber: " + split_user[9])
-                    
-            if check == "2" and usercheck == split_user[2].lower():
-                    print(split_user[0] + ". " + split_user[1] + " " + split_user[2] + "\nStreetaddress: " + split_user[3] + "\nZipcode: " + split_user[4] + "\nCity: " + split_user[5] + "\nEmailaddress: " + split_user[6] + "\nUsername: " + split_user[7] + "\nTelephonenumber: " + split_user[9])
+        isfound = False
+        while isfound == False:
+            usercheck = input(f"\nFill in the {terms[int(check) - 1]}:\n").lower()
             
-            if check == "3" and usercheck == split_user[3].lower():
-                    print(split_user[0] + ". " + split_user[1] + " " + split_user[2] + "\nStreetaddress: " + split_user[3] + "\nZipcode: " + split_user[4] + "\nCity: " + split_user[5] + "\nEmailaddress: " + split_user[6] + "\nUsername: " + split_user[7] + "\nTelephonenumber: " + split_user[9])
-
-            if check == "4" and usercheck == split_user[4].lower():
-                    print(split_user[0] + ". " + split_user[1] + " " + split_user[2] + "\nStreetaddress: " + split_user[3] + "\nZipcode: " + split_user[4] + "\nCity: " + split_user[5] + "\nEmailaddress: " + split_user[6] + "\nUsername: " + split_user[7] + "\nTelephonenumber: " + split_user[9])
             
-            if check == "5" and usercheck == split_user[5].lower():
-                    print(split_user[0] + ". " + split_user[1] + " " + split_user[2] + "\nStreetaddress: " + split_user[3] + "\nZipcode: " + split_user[4] + "\nCity: " + split_user[5] + "\nEmailaddress: " + split_user[6] + "\nUsername: " + split_user[7] + "\nTelephonenumber: " + split_user[9])
+            for user in csvdata:
+                split_user = user[0].split(';') 
+                if check == "1" and usercheck == split_user[1].lower():
+                    isfound = True
+                    print("\nFirstname: " + split_user[1] + "\nSurname: " + split_user[2] + "\nStreetaddress: " + split_user[3] + "\nZipcode: " + split_user[4] + "\nCity: " + split_user[5] + "\nEmailaddress: " + split_user[6] + "\nUsername: " + split_user[7] + "\nTelephonenumber: " + split_user[9])
+                    break
 
-            if check == "6" and usercheck == split_user[6].lower():
-                    print(split_user[0] + ". " + split_user[1] + " " + split_user[2] + "\nStreetaddress: " + split_user[3] + "\nZipcode: " + split_user[4] + "\nCity: " + split_user[5] + "\nEmailaddress: " + split_user[6] + "\nUsername: " + split_user[7] + "\nTelephonenumber: " + split_user[9])
+                if check == "2" and usercheck == split_user[2].lower():
+                    isfound = True
+                    print("\nFirstname: " + split_user[1] + "\nSurname: " + split_user[2] + "\nStreetaddress: " + split_user[3] + "\nZipcode: " + split_user[4] + "\nCity: " + split_user[5] + "\nEmailaddress: " + split_user[6] + "\nUsername: " + split_user[7] + "\nTelephonenumber: " + split_user[9])
+                    break
 
-            if check == "7" and usercheck == split_user[7].lower():
-                    print(split_user[0] + ". " + split_user[1] + " " + split_user[2] + "\nStreetaddress: " + split_user[3] + "\nZipcode: " + split_user[4] + "\nCity: " + split_user[5] + "\nEmailaddress: " + split_user[6] + "\nUsername: " + split_user[7] + "\nTelephonenumber: " + split_user[9])
-            
-            if check == "8"and usercheck == split_user[9].lower():
-                    print(split_user[0] + ". " + split_user[1] + " " + split_user[2] + "\nStreetaddress: " + split_user[3] + "\nZipcode: " + split_user[4] + "\nCity: " + split_user[5] + "\nEmailaddress: " + split_user[6] + "\nUsername: " + split_user[7] + "\nTelephonenumber: " + split_user[9])
-    
+                if check == "3" and usercheck == split_user[3].lower():
+                    isfound = True
+                    print("\nFirstname: " + split_user[1] + "\nSurname: " + split_user[2] + "\nStreetaddress: " + split_user[3] + "\nZipcode: " + split_user[4] + "\nCity: " + split_user[5] + "\nEmailaddress: " + split_user[6] + "\nUsername: " + split_user[7] + "\nTelephonenumber: " + split_user[9])
+                    break
+
+                if check == "4" and usercheck == split_user[4].lower():
+                    isfound = True
+                    print("\nFirstname: " + split_user[1] + "\nSurname: " + split_user[2] + "\nStreetaddress: " + split_user[3] + "\nZipcode: " + split_user[4] + "\nCity: " + split_user[5] + "\nEmailaddress: " + split_user[6] + "\nUsername: " + split_user[7] + "\nTelephonenumber: " + split_user[9])
+                    break
+
+                if check == "5" and usercheck == split_user[5].lower():
+                    isfound = True
+                    print("\nFirstname: " + split_user[1] + "\nSurname: " + split_user[2] + "\nStreetaddress: " + split_user[3] + "\nZipcode: " + split_user[4] + "\nCity: " + split_user[5] + "\nEmailaddress: " + split_user[6] + "\nUsername: " + split_user[7] + "\nTelephonenumber: " + split_user[9])
+                    break
+
+                if check == "6" and usercheck == split_user[6].lower():
+                    isfound = True
+                    print("\nFirstname: " + split_user[1] + "\nSurname: " + split_user[2] + "\nStreetaddress: " + split_user[3] + "\nZipcode: " + split_user[4] + "\nCity: " + split_user[5] + "\nEmailaddress: " + split_user[6] + "\nUsername: " + split_user[7] + "\nTelephonenumber: " + split_user[9])
+                    break
+
+                if check == "7" and usercheck == split_user[7].lower():
+                    isfound = True
+                    print("\nFirstname: " + split_user[1] + "\nSurname: " + split_user[2] + "\nStreetaddress: " + split_user[3] + "\nZipcode: " + split_user[4] + "\nCity: " + split_user[5] + "\nEmailaddress: " + split_user[6] + "\nUsername: " + split_user[7] + "\nTelephonenumber: " + split_user[9])
+                    break
+
+                if check == "8"and usercheck == split_user[9].lower():
+                    isfound = True
+                    print("\nFirstname: " + split_user[1] + "\nSurname: " + split_user[2] + "\nStreetaddress: " + split_user[3] + "\nZipcode: " + split_user[4] + "\nCity: " + split_user[5] + "\nEmailaddress: " + split_user[6] + "\nUsername: " + split_user[7] + "\nTelephonenumber: " + split_user[9])
+                    break
+
+            if not isfound:
+                print("\nNo member found")    
+
     #Delete member
     def deletemember(self):
-        lines = list()
-        members= input("Please enter a member's name to be deleted.")
         with open("data/members.csv", "r") as f:
-            reader = csv.reader(f)
-            for row in reader:
-                lines.append(row)
-                for mem in row:
-                    if mem == members:
-                        lines.remove(row)
+            csvdata = list(csv.reader(f))
 
-        with open('mycsv.csv', 'w') as writeFile:
-            writer = csv.writer(writeFile)
-            writer.writerows(lines)
+        running = True
+        while running:
+            user_found = False
+            username = input("Please enter the username of the member you want to delete: ").lower()
+            
+            with open("data/members.csv", "w", newline='') as f:
+                writer = csv.writer(f)
+                index = 0
+                for user in csvdata:
+                    thisuser = user[0].split(';')
+                    if username == thisuser[7].lower():
+                        print("\nUser found! Deleting...\n")
+                        user_found = True
+                    elif username != thisuser[7].lower():
+                        if index == 0:
+                            writer.writerow(user)
+                            index += 1
+                        else:
+                            new_index = thisuser
+                            new_index[0] = str(index)
+                            joined_new_index = ';'.join(new_index)
+                            final_new_index = [joined_new_index]
+                            writer.writerow(final_new_index)
+                            index += 1
+
+            if user_found == True:
+                running = False
     
     #Show all members
     def showAllmembers(self):
@@ -110,7 +150,7 @@ class Person():
                     memlist = i.split(';')
                     print(memlist[0] + ". " + memlist[1] + " " + memlist[2])
                 index += 1
-
+        
     #Edit member
     def editmember(self):
         with open("data/members.csv", "r") as f:
@@ -128,114 +168,31 @@ class Person():
 
         active = True
         while active:
-            exists = False	
+            exists = False
             username = input("Please enter the username of the member you want to edit: ").lower()
-            for user in csvdata:
-                thisuser = user[0].split(';')
-                if username == thisuser[7].lower():
-                    print("User found!")
-                    exists = True
-                    new_value = input("What new value do you want to insert?")
+            
+            with open("data/members.csv", "w", newline='') as f:
+                writer = csv.writer(f)
+                for user in csvdata:
+                    thisuser = user[0].split(';')
+                    if username != thisuser[7].lower():
+                        writer.writerow(user)
+                    if username == thisuser[7].lower():
+                        print("User found!")
+                        new_value = input("What new value do you want to insert?")
 
-                    with open ('data/members.csv', 'w') as f:
-                        writer = csv.writer(f)
-                        index = thisuser[0]
-                        s = ""
-                        for row in csvdata:
-                            row_list = row[0].split(';')
-                            print(index)
-                            print(row_list[0])
-                            if row_list[0] == index:
-                                for i in range(len(thisuser)):
-                                    if i == int(check) and i == len(thisuser) - 1:
-                                        s += new_value
-                                    elif i == int(check) and i != len(thisuser) - 1:
-                                        s += new_value + ";"
-                                    elif i == len(thisuser) - 1:
-                                        s += thisuser[i]
-                                    else: 
-                                        s += thisuser[i] + ";"
-                                print("s = ", s)
-                                for line in csvdata:
-                                    line_list = line[0].split(';')
-                                    if line_list[0] == index:
-                                        writer.writerow(s)
-                                
+                        found_user = thisuser
+                        found_user[int(check)] = new_value
+                        joined_user = ";".join(found_user)
+                        final_user = [joined_user]
+                        writer.writerow(final_user)
+                        exists = True
             if exists:
+                print("\nSuccefully updated member ", username, "!\n")
                 active = False
-
-                
-                    #     index = int(thisuser[0])
-                    #     thisuser[int(check)] = new_value
-                    #     s = ""
-                    #     for i in range(len(thisuser)):
-                    #         if i == len(thisuser) - 1:
-                    #             s += thisuser[i]
-                    #             break
-                    #         s += thisuser[i] + ";"
-                    #     f[index].write(s)
-                    # active = False
-                    
-        
-        # while running:
-        #     exists = False
-        #     username = input("Username: ").lower()
-        #     with open("data/members.csv", "r") as f:
-        #         csvdata = list(csv.reader(f))
-        #     for user in csvdata:
-        #         split_user = user[0].split(';') 
-        #         if username == split_user[7].lower():
-        #             exists = True
-        #             print("This username is already in use.")
-        #             break
-        #     if exists == False:
-        #         running = False
-        
-        
-    def EditMember(self, check, username):
-        csvdata = open('data/members.csv', 'r+')
-        terms = ['Givenname', 'Surname', 'Streetaddress', 'Zipcode', 'City', 'Emailaddress', 'Username', 'Telephonenumber']
-        for user in csvdata:
-            thisuser = user.split(';')
-            if username == thisuser[7]:
-
-                if check == "1":
-                    print("\nThe current Givenname is: " + thisuser[1])
-                    thisuser[1] = input(f"\nFill in the new {terms[int(check)-1]}:\n").lower()
-                    
-
-                elif check == "2":
-                    thisuser[2] = input(f"\nFill in the new {terms[int(check)-1]}:\n").lower()
-                    break
-
-                elif check == "3":
-                    thisuser[3] = input(f"\nFill in the new {terms[int(check)-1]}:\n").lower()
-                    break
-
-                elif check == "4":
-                    thisuser[4] = input(f"\nFill in the new {terms[int(check)-1]}:\n").lower()
-                    break
-
-                elif check == "5":
-                    thisuser[5] = input(f"\nFill in the new {terms[int(check)-1]}:\n").lower()
-                    break
-
-                elif check == "6":
-                    thisuser[6] = input(f"\nFill in the new {terms[int(check)-1]}:\n").lower()
-                    break
-
-                elif check == "7":
-                    thisuser[7] = input(f"\nFill in the new {terms[int(check)-1]}:\n").lower()
-                    break
-                
-                elif check == "8":
-                    thisuser[8] = input(f"\nFill in the new {terms[int(check)-1]}:\n").lower()
-                    break
-
-                thisuser = ';'.join(thisuser)
-                csvdata.write(thisuser)
-                break
-        csvdata.close()
+            else:
+                print("\nMember not found\n")
+                print("\nPlease enter an existing member\n")
 
     #add list of members
     def addListOfMembers(self):
@@ -243,10 +200,21 @@ class Person():
         filepath = self.findfile(filename, "/")
         with open(filepath, "r") as f:
             csvdata = list(csv.reader(f))
-        with open("data/members.csv", "a", newline="") as f:
+        with open("data/members.csv", "r") as f:
+            csvdata2 = list(csv.reader(f))
+            index = len(csvdata2)
+        with open("data/members.csv", "a", newline='') as f:
             writer = csv.writer(f)
-            for user in csvdata:
-                writer.writerow(user)
+            for idx, user in enumerate(csvdata):
+                if idx != 0:
+                    splituser = user[0].split(';')
+                    splituser[0] = str(index)
+                    joineduser = ";".join(splituser)
+                    finaluser = [joineduser]
+                    writer.writerow(finaluser)
+                    index += 1
+                
+                
         print("\nThe file has been added to the database.")
     
     def findfile(self, name, path):
